ccd9b293e88d2065ffaa5af4f563d44625c4a811
HongjianZhu <zhu.life@gmail.com>
2018-12-19 13:33:35 +0800

    add idle & open conn conf

--- db/db.go
+++ db/db.go
@@ -12,10 +12,12 @@ var (
 	db       *sql.DB
 )
 
-func Open(t, dsn string) error {
+func Open(t, dsn string, openConn, idleConn int) error {
 	var err error
 	openOnce.Do(func() {
 		db, err = sql.Open(t, dsn)
+		db.SetMaxOpenConns(openConn)
+		db.SetMaxIdleConns(idleConn)
 	})
 	if err != nil {
 		return err
